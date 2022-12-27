f9a4043fef2ff8d798c676c9f0c9189dcfc9a18c
Salih Ahmet <salihahmet.nuhoglu@gmail.com>
2022-12-27 21:52:32 +0300

    add todo and isdone

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import React, { useState } from "react";
 function App() {
 
   const [todoText, setTodoText] = useState("");
-  
-  const handleSubmit = (event) =>{
+  const [todos, setTodos] = useState([]);
+
+  const changeIsDone = (id) => {
+    const searchedTodo = todos.find((item) => item.id === id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    setTodos([updatedTodo, ...filteredTodos]);
+  }
+
+  const handleSubmit = (event) => {
     event.preventDefault()
-    if(todoText === "") {
+    if (todoText === "") {
       alert("Todo text can't be empty!")
+      return;
     }
-    
+    const hasTodos=todos.find((item) => item.text === todoText);
+    if(hasTodos !== undefined) {
+      alert("You have the todo already");
+      return;
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      isDone: false,
+      text: todoText,
+      date: new Date(),
+    };
+    setTodos([newTodo, ...todos]);
+    console.log(newTodo);
   }
 
   return (
@@ -31,6 +56,22 @@ function App() {
           </button>
         </div>
       </form>
+      {
+        todos.length <= 0 ? (
+          <p className="text-center my-5">You don't have any todos yet</p>
+        ) : (
+          <>
+            {
+              todos.map(item => (
+                <div class="alert alert-secondary d-flex justify-content-between align-items-center" role="alert">
+                  <p>{item.text}</p>
+                  <button onClick={() => changeIsDone(item.id)} className="btn btn-sm btn-secondary">{item.isDone === false ? "Done" : "Undone"}</button>
+                </div>
+              ))
+            }
+          </>
+        )
+      }
     </div>
   );
 }
